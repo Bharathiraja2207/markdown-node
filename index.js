3885a44c1e398542ffd86f5d86e0935353aef57b
Bharathiraja2207 <bharathiraja2207@gmail.com>
2023-03-28 13:19:48 +0530

    markdown

--- index.js
+++ index.js
@@ -3,18 +3,21 @@ import express from "express"; // "type": "module"
 import { MongoClient } from "mongodb";
 import cors from "cors";
 import bodyParser from "body-parser";
-import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import * as dotenv from 'dotenv'
+
 dotenv.config()
 const app = express();
 // const mongo_url = 'mongodb://127.0.0.1';
 const mongo_url =(process.env.mongo_url)
 export const client = new MongoClient(mongo_url);
 await client.connect();
+
   console.log('mongo is connected!!');
 
   app.use(express.json())
   app.use(bodyParser.json())
   app.use(cors())
+
 const PORT = (process.env.PORT)
 // const PORT = 4000;
 
